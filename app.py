936ca99126bc276cae50f056f4cc0bc53b23caa1
rupali-bari-1975 <gauravsangrah1975@gmail.com>
2025-05-13 15:36:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.font_manager as fm
 import seaborn as sns
 from datetime import datetime, timedelta
 from github import Github
